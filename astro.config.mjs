cf995e26ce7421dab8b1db188c0a23c8db0beefe
fmagnone <fmagnone@gmail.com>
2025-04-13 12:59:36 +0200

    Update deploy file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fmruy.github.io',
-  base: '/js-to-ts',
+  site: 'https://fmruy.github.io/js-to-ts',
+  base: '/',
 });
